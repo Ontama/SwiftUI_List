cecdb1f5d82e95f86c7b4d1635e7bf229d7b7b10
ontama154 <onchanvvv@gmail.com>
2022-03-07 20:36:58 +0900

    Initial Commit

---
+++ Shared/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  Shared
+//
+//  Created by tomoyo_kageyama on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
